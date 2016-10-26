8fd9581a22fb56136d294de01911d3392f5b90f9
jamesthechamp <bajajshivansh1@gmail.com>
2016-10-27 04:29:39 +0530

    database intergrated

--- server.js
+++ server.js
@@ -5,28 +5,44 @@ app.use(express.static('public'));
 var mongoose = require('mongoose');
 // Connection URL. This is where your mongodb server is running.
 var url = 'mongodb://james:james@ds031607.mlab.com:31607/complainportal';
-//mongoose.connect(url);
+var sql_url="sql6.freemysqlhosting.net:3306"
+//var con=mysql.createConnection({
+//	host: "sql6.freemysqlhosting.net:3306/",
+//	user:"sql6141657",
+//	password: "L9yBHXjJdb"
+//});
+//con.connect(function(err){
+//  if(err){
+//    console.log('Error connecting to Db',err);
+//    return;
+//  }
+//  console.log('Connection established');
+//});
+mongoose.connect(url, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected',url);
+});
 // Use connect method to connect to the Server
-//var Schema = mongoose.Schema;
+var Schema = mongoose.Schema;
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 // create a schema
-//var complainSchema = new Schema({
-//  name: String,
-//  email: String,
-//  date: Date,
-//  hostel: String,
-//  issue: String,
-//  type: String,
-//  Status: String,
-//  rollno:String
-//});
-//var Complain=mongoose.model('complain',complainSchema);
-//module.exports=Complain;
-//var db = mongoose.connection;
-//db.on('error', console.error.bind(console, 'connection error:'));
-//db.once('open', function() {
-//  console.log("we're connected!");
-//});
+var complainSchema = new Schema({
+  name: String,
+  email: String,
+  date: Date,
+  hostel: String,
+  issue: String,
+  type: String,
+  Status: String,
+  rollno:String
+});
+var Complain=mongoose.model('complain',complainSchema);
+module.exports=Complain;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected!");
+});
 app.get('/', function (req, res) {
    res.sendFile( __dirname + "/templates/" + "index.html" );
 })
@@ -52,21 +68,21 @@ app.get('/profile.htm', function (req, res) {
    res.sendFile( __dirname + "/templates/" + "profile.html" );
 })
 app.post('/saveComplain',urlencodedParser, function(req, res){
-//   data = Complain({
-//      name:req.body.name,
-//      email:req.body.email,
-//      date: Date.now(),
-//      type: req.body.type,
-//      hostel: req.body.hostel,
-//      issue: req.body.issue,
-//      rollno: req.body.rollno,
-//      Status: "left"
-//   });
-//   data.save(function(err) {
-//  if (err) throw err;
+   data = Complain({
+      name:req.body.name,
+      email:req.body.email,
+      date: Date.now(),
+      type: req.body.type,
+      hostel: req.body.hostel,
+      issue: req.body.issue,
+      rollno: req.body.rollno,
+      Status: "left"
+   });
+   data.save(function(err) {
+  if (err) throw err;
 
-//  console.log('Complain Added');
-//  });
+  console.log('Complain Added');
+  });
   res.redirect('/');
 });
 var server = app.listen(process.env.PORT || 8081, function () {
